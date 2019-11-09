7eedf4b8cba1be84873901ce665bc63e54a20c10
A-LoneWolf <54808110+A-LoneWolf@users.noreply.github.com>
2019-11-09 16:01:11 +0530

    Update and rename deepfry.py to xyztool.py

--- stdplugins/xyztool.py
+++ stdplugins/xyztool.py
@@ -1,5 +1,7 @@
 """Reply to any media sticker It will fry it and send
 Command: `.df` <optional number 1-8>
+Reply any Image with `.thm` to generate Custom theme of that image
+
 """
 
 import datetime
@@ -37,3 +39,35 @@ async def _(event):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
           else: 
              await borg.send_file(event.chat_id, response.message.media)
+
+
+
+@borg.on(admin_cmd("thm ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```reply to text message```")
+       return
+    chat = "@ThemerBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=864838521))
+              await borg.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Hi"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await borg.send_file(event.chat_id, response.message)
