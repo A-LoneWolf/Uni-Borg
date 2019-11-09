0b075e97fa8b5827a0e5bd208b0a29617afe2365
A-LoneWolf <54808110+A-LoneWolf@users.noreply.github.com>
2019-11-09 13:38:13 +0530

    Update and rename gaand_me_loge.py to fun2.py

---
+++ stdplugins/fun2.py
@@ -0,0 +1,66 @@
+"""Syntax: `.gaand`
+           `.eye`
+
+"""
+from telethon import events
+import asyncio
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 16)
+    input_str = event.pattern_match.group(1)
+    if input_str == "gaand":
+        await event.edit(input_str)
+        animation_chars = [
+            "me",
+            "loge",
+            "kya?",
+            "gaand"
+        ]
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 4])
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "eye":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "👁👁\n  👄  =====> Abey Ja Na Gandu",
+            "👁👁\n  👅  =====> Abey Ja Na Madarchod",    
+            "👁👁\n  💋  =====> Abey Ja Na Randi",
+            "👁👁\n  👄  =====> Abey Ja Na Betichod",
+            "👁👁\n  👅  =====> Abey Ja Na Behenchod",    
+            "👁👁\n  💋  =====> Abey Ja Na Na Mard",
+            "👁👁\n  👄  =====> Abey Ja Na Randi",
+            "👁👁\n  👅  =====> Abey Ja Na Bhosdk",    
+            "👁👁\n  💋  =====> Abey Ja Na Chutiye",
+            "👁👁\n  👄  =====> Hi All, How Are You Guys..."
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
