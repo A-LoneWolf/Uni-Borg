53bbe351d036f76f785e6ba4034802a712613f98
A-LoneWolf <54808110+A-LoneWolf@users.noreply.github.com>
2019-11-20 17:41:35 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -1,8 +1,8 @@
 """Personal Message Spammer
 Available Commands:
-.approve
-.block
-.list approved pms"""
+`.approve`
+`.blockpm`
+`.listpms`"""
 import asyncio
 import json
 from telethon import events
@@ -15,10 +15,10 @@ borg.storage.PM_WARNS = {}
 borg.storage.PREV_REPLY_MESSAGE = {}
 
 
-BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
-TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
-UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = "Hi! I will answer to your message soon. Please wait for my response and don't spam my PM. Thanks"
+BAALAJI_TG_USER_BOT = "`My Master hasn't approved you to PM.`"
+TG_COMPANION_USER_BOT = "`Please wait for his response and don't spam his PM.`"
+UNIBORG_USER_BOT_WARN_ZERO = "`I am currently offline. Please do not SPAM me.`"
+UNIBORG_USER_BOT_NO_WARN = "`Hi! I will answer to your message soon. Please wait for my response and don't spam my PM. Thanks`"
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
@@ -53,7 +53,7 @@ async def monito_p_m_s(event):
             borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
 
 
-@borg.on(admin_cmd("approvepm ?(.*)"))
+@borg.on(admin_cmd("approve ?(.*)"))
 async def approve_p_m(event):
     if event.fwd_from:
         return
@@ -88,7 +88,7 @@ async def approve_p_m(event):
                 await borg(functions.contacts.BlockRequest(chat.id))
 
 
-@borg.on(admin_cmd("list approved pms"))
+@borg.on(admin_cmd("listpms"))
 async def approve_p_m(event):
     if event.fwd_from:
         return
